236f1938b124edf63ae09f42950a553ee351e244
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-06-23 20:01:16 +0100

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,1 +1,4 @@
 # Shared functions for filters, formatting, etc.
+def format_datetime(dt_string):
+    from datetime import datetime
+    return datetime.strptime(dt_string, "%Y-%m-%dT%H:%M:%S.%fZ").strftime("%d %b %Y %I:%M %p")
