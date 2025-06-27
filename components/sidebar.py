b5b3deb58c26a34de669b1e8c5fe42ef15db9fcb
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-06-27 02:23:11 +0100

    Create sidebar.py

---
+++ components/sidebar.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+def sidebar():
+    if "logged_in" not in st.session_state:
+        st.session_state.logged_in = False
+
+    with st.sidebar:
+        if st.session_state.get("logged_in") and "user" in st.session_state:
+            user = st.session_state["user"]
+            full_name = user.get("fullname") or user.get("email", "User").split("@")[0].capitalize()
+            st.success(f"👋 Welcome, {full_name}!")
+
+        st.info("**.....learning from the best, becoming the best!**\n\n"
+                "MentorLink ....your sure path to a better tomorrow.")
+
+        st.markdown("**📞 Contact Us:**")
+        st.markdown("- [💬 Chat with the Support Team](https://wa.me/2348062529172)")
+        st.markdown("---")
+        st.markdown("### 👨‍💻 App Developer")
+        st.markdown(
+            """
+**Ezekiel BALOGUN**  
+_Full-Stack Developer_  
+_Data Scientist / Analyst_  
+_AI / Machine Learning Engineer_  
+_Automation / BI Expert_
+
+📧 [ezekiel4true@yahoo.com](mailto:ezekiel4true@yahoo.com)  
+🔗 [LinkedIn](https://www.linkedin.com/in/ezekiel-balogun-39a14438)  
+📞 +2348062529172
+            """
+        )
