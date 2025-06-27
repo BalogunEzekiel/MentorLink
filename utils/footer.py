26c11520b57ffab73fe7cb1e9f53d4fdf684dece
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-06-27 12:51:59 +0100

    Update footer.py

--- utils/footer.py
+++ utils/footer.py
@@ -1,26 +1,22 @@
 import streamlit as st
+import os
 
 def app_footer():
     st.markdown("---")
     st.markdown("### 🤝 Supporters", unsafe_allow_html=True)
 
+    asset_dir = "assets"
     logos = [
-        "assets/BosunTijani1.jpg",
-        "assets/Partner_3MTT.png",
-        "assets/Partner_NITDA.jpg",
-        "assets/Partner_DSN.png",
-        "assets/Partner_DeepTech_Ready.png",
-        "assets/Partner_Incubator.png",
-        "assets/Partner_DSA.jpg",
-        "assets/Partner_Google3.png",
-        "assets/Partner_Microsoft2.png"
+        "Partner_Incubator.png",
+        "Partner_DSA.jpg",
     ]
 
     for i in range(0, len(logos), 4):
         cols = st.columns(4)
         for j, logo in enumerate(logos[i:i+4]):
             with cols[j]:
+                path = os.path.join(asset_dir, logo)
                 try:
-                    st.image(logo, width=120)
+                    st.image(path, width=120)
                 except Exception as e:
-                    st.warning(f"Could not load logo: {e}")
+                    st.warning(f"Could not load logo: {logo}")
