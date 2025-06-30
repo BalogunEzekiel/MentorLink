c96dbd1106ee330bf5bbc49f96c5a8f8d1e15fa1
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-06-30 11:30:08 +0100

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,19 +1,19 @@
 # admin.py
 
-import streamlit as st
-from admin import users, matches, sessions
+#import streamlit as st
+#from admin import users, matches, sessions
 
-def show():
-    st.title("👨‍💼 Admin Dashboard")
-    st.info("Manage users, match mentors & mentees, and oversee sessions.")
+#def show():
+#    st.title("👨‍💼 Admin Dashboard")
+#    st.info("Manage users, match mentors & mentees, and oversee sessions.")
 
-    tab1, tab2, tab3 = st.tabs(["🧑‍💼 Users", "🔁 Matches", "📅 Sessions"])
+#    tab1, tab2, tab3 = st.tabs(["🧑‍💼 Users", "🔁 Matches", "📅 Sessions"])
 
-    with tab1:
-        users.show()
+#    with tab1:
+#        users.show()
 
-    with tab2:
-        matches.show()
+#    with tab2:
+#        matches.show()
 
-    with tab3:
-        sessions.show()
+#    with tab3:
+#        sessions.show()
