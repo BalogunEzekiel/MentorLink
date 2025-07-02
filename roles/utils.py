991982db33fe06a700120c0f0d432c6748b5f3a6
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-07-02 13:59:49 +0100

    Delete roles/utils.py

--- roles/utils.py
+++
@@ -1,8 +0,0 @@
-# utils.py
-from datetime import datetime
-
-def format_datetime(dt_string):
-    try:
-        return datetime.fromisoformat(dt_string).strftime("%Y-%m-%d %H:%M")
-    except Exception:
-        return dt_string  # return raw string if formatting fails
