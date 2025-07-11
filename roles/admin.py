d6161bb3b6d6819e4e18495a1e61d5690c036c13
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-07-11 07:41:49 +0100

    Update admin.py

--- roles/admin.py
+++ roles/admin.py
@@ -7,6 +7,7 @@ import streamlit as st
 import pandas as pd
 import pytz
 import plotly.express as px
+import uuid
 
 # Adjust path for imports
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
